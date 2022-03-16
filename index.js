adf2aaaab47ea3b8227c65a6a11537aa0e551978
Erika Williams <erika@erikawilliams.us>
2022-03-16 16:07:13 -0600

    commit tree eb084c17635a6a11844fb8a9f8b317242260499c

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+function superbowlWin(array) {
+    let win = array.find(isWin);
+    // console.log("testing suberbowlWin")
+    // console.log(win)
+    if (win === undefined){
+        // console.log(win)
+        return win;
+    } else {
+        // console.log(win["year"])
+        return win["year"]
+    }
+}
+
+function isWin(element,index,array){
+    // console.log("testing isWin")
+    // console.log(element["result"]==="W")
+    return element["result"] === "W"
+}
+
+superbowlWin(record);
